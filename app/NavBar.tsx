a72fa9ec87c7d68fe68655c497f756928c927013
a-nayak98 <nabhi977756@gmail.com>
2024-08-01 13:48:38 +0530

    adding styles to the active nav links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { FaBugSlash } from "react-icons/fa6";
+import { usePathname } from 'next/navigation';
+import classNames from 'classnames';
 
 const NavBar = () => {
+    const routName = usePathname()
+    // console.log(routName)
     const links = [{label:"Dashboard", href:"/"},{label:"Issues", href:"/issues"}]
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
@@ -10,7 +15,11 @@ const NavBar = () => {
         <ul className='flex space-x-6'>
             {links.map((elm) =>{
                 return <><li key={elm.href}>
-                    <Link  className='text-zinc-500 hover:text-zinc-800 transition-colors' href={elm.href}>{elm.label}</Link>
+                    <Link  className={classNames({
+                        "text-zinc-900" : elm.href === routName,
+                        "text-zinc-500" : elm.href !== routName,
+                        "hover:text-zinc-800 transition-colors":true
+                    })} href={elm.href}>{elm.label}</Link>
                     </li></>
             })}
         </ul>
